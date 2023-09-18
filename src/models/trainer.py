93572d2f92c8391802ea643aeeb0ed6815355b87
Alan Blanchet <alan@alan-blanchet.fr>
2023-09-18 16:51:39 +0200

    [DEV]Added a config
    Added a config and mlflow regulator

--- src/models/trainer.py
+++ src/models/trainer.py
@@ -7,7 +7,12 @@ from transformers import Trainer
 
 
 class SOTrainer(Trainer):
+    def __init__(self, *args, **kwargs):
+        self.use_mlflow = kwargs.pop("mlflow", True)
+        super(SOTrainer, self).__init__(*args, **kwargs)
+
     def log(self, logs: dict[str, float]) -> None:
         logs["learning_rate"] = self._get_learning_rate()
-        mlflow.log_metric("learning_rate", logs["learning_rate"])
+        if self.use_mlflow:
+            mlflow.log_metric("learning_rate", logs["learning_rate"])
         super().log(logs)
